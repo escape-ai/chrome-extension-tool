b388e20a96fac952bbf60376819d64bea9b9e8c2
Krishna Penukonda <penukonda.krishna.moorthy@gmail.com>
2019-04-20 23:44:25 +0800

    Fix XHR permissions

--- background.js
+++ background.js
@@ -4,21 +4,37 @@
 
 'use strict';
 
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
+const API_ROOT = "http://fakerinos.herokuapp.com/api/";
+
+function fetchDomains() {
+    let xmlhttp = new XMLHttpRequest();
+    const domain_endpoint = API_ROOT + "articles/domain/";
+    xmlhttp.open("GET", domain_endpoint, true);
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState === 4) {
+            const domains = JSON.parse(xmlhttp.responseText);
+            chrome.storage.sync.set({domains: domains});
+            console.log(domains.toString());
+        }
+    };
+    xmlhttp.send();
+}
+
+chrome.runtime.onInstalled.addListener(function () {
+    chrome.storage.sync.set({color: '#3aa757'}, function () {
         console.log("The color is green.");
     });
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    fetchDomains();
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
         chrome.declarativeContent.onPageChanged.addRules([{
             conditions: [
                 new chrome.declarativeContent.PageStateMatcher({
-                    pageUrl: {hostEquals: 'politics.theonion.com'},
-                }),
-                new chrome.declarativeContent.PageStateMatcher({
-                    pageUrl: {hostEquals: 'www.*theonion.com'},
+                    // pageUrl: {hostEquals: '*'},
                 })
             ],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
+            actions: [
+                new chrome.declarativeContent.ShowPageAction(),
+            ]
         }]);
     });
 });
--- js/popup.js
+++ js/popup.js
@@ -21,16 +21,16 @@ function extract_hostname(url) {
     return a.hostname;
 }
 
-changeColor.onclick = function(element) {
+changeColor.onclick = function (element) {
     let color = element.target.value;
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
         chrome.tabs.executeScript(
             tabs[0].id,
             {code: 'document.body.style.backgroundColor = "' + color + '";'});
     });
 };
 
-chrome.storage.sync.get('color', function(data) {
+chrome.storage.sync.get('color', function (data) {
     changeColor.style.backgroundColor = data.color;
     changeColor.setAttribute('value', data.color);
 });

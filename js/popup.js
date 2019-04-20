29d3dab2442e29b34a01b1d5d4e11b5d6e26480f
Krishna Penukonda <penukonda.krishna.moorthy@gmail.com>
2019-04-21 04:34:13 +0800

    Article Fetch is Work.

--- js/popup.js
+++ js/popup.js
@@ -1,13 +1,5 @@
 let changeColor = document.getElementById('changeColor');
 
-// var site = location.hostname,
-//     title = document.title;
-//
-// // console.log("Site: " + site + " - Title: " + title);
-//
-// console.log("lalalalalaa");
-
-
 function check_title(title) {
     title = title.toString().replace(/\s+/g, ' ').trim();
     var title_display = document.getElementById("title-display");
@@ -15,28 +7,11 @@ function check_title(title) {
     title_display.innerHTML = title.toString();
 }
 
-function extract_hostname(url) {
-    var a = document.createElement('a');
-    a.href = url;
-    return a.hostname;
-}
+chrome.storage.sync.get('domains', function (domains) {
 
-changeColor.onclick = function (element) {
-    let color = element.target.value;
-    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-        chrome.tabs.executeScript(
-            tabs[0].id,
-            {code: 'document.body.style.backgroundColor = "' + color + '";'});
-    });
-};
-
-chrome.storage.sync.get('color', function (data) {
-    changeColor.style.backgroundColor = data.color;
-    changeColor.setAttribute('value', data.color);
 });
 
-ocument.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function () {
     var title_display = document.getElementById("title-display");
     title_display.innerHTML = "lalalala";
-
-})
+});
